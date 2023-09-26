c07973e1a8bae6cb0f50afbb3d7e31bf959e3889
abbasghazal <134511359+abbasghazal@users.noreply.github.com>
2023-09-26 09:22:08 +0300

    Add files via upload

---
+++ OWNER.py
@@ -0,0 +1,10 @@
+# متطلبات التنصيب #
+OWNER = ["Elasyoutyy","A_q_lp"]
+OWNER_NAME = "||『مـم ـ ـول علـواآ الاسيوطيي』||"
+BOT_TOKEN = "6399913204:AAFZr7emDB764ZevDh-LE8pXtf8wVT8DaT0"
+DATABASE = "mongodb+srv://Elkber:Elkber@cluster0.feuljpn.mongodb.net/?retryWrites=true&w=majority"
+CHANNEL = "https://t.me/Elasyoutyyyy"
+GROUP = "https://t.me/Elasyoutyy2"
+VIDEO = "https://telegra.ph/file/770e2d5df0b50264097b2.jpg"
+LOGS = "Elasyoutyy2"
+
